28e7459e112797ae34d13edccc7651ed6d42ba7e
github-username <mail@gmail.com>
2022-11-19 14:58:05 +0100

    feat: add showBirdItemDescription function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,66 @@
-import {birdsDataBy} from './js/birds.js'
-
- const answerItems = document.querySelectorAll('.answer-list__item')
- 
- let questionIndex = 0; // current question
- let score = 0;
- let currentBirdVoice = new Audio();
+import { birdsDataBy } from './js/birds.js'
 
 
- showQuestion()
+let questionIndex = 0; // current question
+let score = 0;
+let currentBirdVoice = new Audio();
+let currentItemSong = new Audio()
+let wrongItemSong = new Audio
 
- function getRandomNum () {
-    return Math.floor((Math.random() * 6));
+/* BirdCard settings */
+
+const cardBirdName = document.querySelector('.card__name');
+const cardBirdImage = document.querySelector('.card__img');
+const cardBirdSoecies = document.querySelector('.card__soecies');
+const cardBirdText = document.querySelector('.card__text');
+const birdItem = document.querySelectorAll('.answer-list__item')
+
+
+showQuestion()
+
+function getRandomNum() {
+  return Math.floor((Math.random() * 6));
+}
+
+
+function showQuestion() {
+  let currentQuestionItem = birdsDataBy[questionIndex];
+  let randomAnswerOfQuestion = currentQuestionItem[getRandomNum()];
+  currentBirdVoice.src = randomAnswerOfQuestion.audio;
+
+  birdItem.forEach((item) => {
+    item.addEventListener('click', showBirdItemInformation)
+  })
+
+  function showBirdItemInformation() {
+    const birdCard = document.querySelector(".card")
+    const instruction = document.querySelector(".instruction")
+    birdCard.classList.remove("card-display")
+    instruction.classList.add("instruction-display")
+    let birdItemName = this.querySelector(".bird-title")
+    for (let i = 0; i < currentQuestionItem.length; i++) {
+      if (birdItemName.textContent === currentQuestionItem[i].name) {
+        cardBirdImage.src = currentQuestionItem[i].image;
+        cardBirdName.textContent = currentQuestionItem[i].name;
+        cardBirdSoecies.textContent = currentQuestionItem[i].species;
+        cardBirdText.textContent = currentQuestionItem[i].description
+      }
+    }
   }
 
+}
 
-  function showQuestion () {
-    let currentQuestion = birdsDataBy[questionIndex];
-    let randomAnswerOfQuestion = currentQuestion[getRandomNum()];
-    currentBirdVoice.src = randomAnswerOfQuestion.audio;
-    console.log(randomAnswerOfQuestion)
-  }
 
- 
 
-  /* Audio */
+function showRightAnswer() {
 
-  let audioPlayQuestion = document.querySelector('.question__player-icon')
-  audioPlayQuestion.addEventListener('click', playAudioQuestion);
+}
+
+
+/* Audio */
+
+let audioPlayQuestion = document.querySelector('.question__player-icon')
+audioPlayQuestion.addEventListener('click', playAudioQuestion);
 
 
 let currentTime = 0;
@@ -34,21 +68,21 @@ let isPlay = false;
 let audioBird = new Audio();
 
 
-  function playAudioQuestion() {
-    audioBird.currentTime = 0;
-    audioBird.src = currentBirdVoice.src;
-    if (!isPlay) {
-      audioBird.play();
-        isPlay = true;
-        audioPlayQuestion.classList.add("pause");
-      } else {
-        audioBird.pause();
-        isPlay = false;
-        audioPlayQuestion.classList.remove("pause");
-      }
+function playAudioQuestion() {
+  audioBird.currentTime = 0;
+  audioBird.src = currentBirdVoice.src;
+  if (!isPlay) {
+    audioBird.play();
+    isPlay = true;
+    audioPlayQuestion.classList.add("pause");
+  } else {
+    audioBird.pause();
+    isPlay = false;
+    audioPlayQuestion.classList.remove("pause");
   }
+}
 
-  //click on timeline to skip around
+//click on timeline to skip around
 
 const timeline = document.querySelector(".timeline");
 const rewindTimline = (el) => {
@@ -99,9 +133,10 @@ const volumeIcon = document.querySelector(".volume-icon");
 volumeIcon.addEventListener("click", () => {
   audioBird.muted = !audioBird.muted;
   if (!audioBird.muted) {
-    volumeIcon.classList.add("mute");
-  } else {
     volumeIcon.classList.remove("mute");
+
+  } else {
+    volumeIcon.classList.add("mute");
   }
 });
 
